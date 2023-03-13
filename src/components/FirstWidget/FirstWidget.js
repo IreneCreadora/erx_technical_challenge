bc5d95e6bad0b83fe09f54d4b6a903cfb63f8070
IreneCreadora <nargiza.dancer@gmail.com>
2023-03-13 18:26:04 +0000

    Update FirstWidget.js

--- src/components/FirstWidget/FirstWidget.js
+++ src/components/FirstWidget/FirstWidget.js
@@ -1,14 +1,26 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { Chart } from 'components/Chart/Chart';
 import { dummySignal } from 'lib/data/measurementData';
 
 export const FirstWidget = () => {
+  const [userReady, setUserReady] = useState(false);
+
+  function handleClick() {
+    setUserReady(true);
+  }
   return (
     <div className="w-360 h-640 flex justify-center items-center m-1 p-4  border border-solid border-red-500/75 rounded bg-amber-100">
       <h1>Code</h1>
-      <div className="w-full">
-        <Chart userData={dummySignal} />
-      </div>
+      {userReady === true ? (
+        <div className="w-full">
+          <Chart userData={dummySignal} />
+        </div>
+      ) : (
+        <></>
+      )}
+      <button type="button" onClick={handleClick} className="w-full bg-red-500">
+        click
+      </button>
     </div>
   );
 };
