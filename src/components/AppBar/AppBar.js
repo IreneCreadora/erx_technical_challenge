c3747d4ea323587e0cb2216e96a3a90059684e0b
IreneCreadora <nargiza.dancer@gmail.com>
2023-03-13 18:06:07 +0000

    feature App bar

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -2,10 +2,8 @@ import { Navigation } from '../Navigation/Navigation';
 
 export const AppBar = () => {
   return (
-    <header className="relative flex flex-wrap items-center justify-between px-2 py-2 dark:bg-slate-800 mb-3 shadow-md">
-      <nav className={`lg:flex flex-grow items-center`}>
-        <Navigation />
-      </nav>
+    <header className="relative flex justify-center items-center px-2 py-2 bg-teal-700 mb-3 shadow-md">
+      <Navigation />
     </header>
   );
 };
