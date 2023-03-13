7606cec4a273d0bf6a144fbd29ea7559001fadb4
IreneCreadora <nargiza.dancer@gmail.com>
2023-03-13 18:02:44 +0000

    add dev dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        360: '360px',
+      },
+      height: {
+        640: '640px',
+      },
+      colors: {
+        accentBg: 'rgba(0, 0, 0, 0.3)',
+      },
+    },
   },
   plugins: [],
 };
