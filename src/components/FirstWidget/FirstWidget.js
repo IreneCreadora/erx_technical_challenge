1748a4bba6d20a052470cb96190552873ae62437
IreneCreadora <nargiza.dancer@gmail.com>
2023-03-13 18:08:53 +0000

    update first widget

--- src/components/FirstWidget/FirstWidget.js
+++ src/components/FirstWidget/FirstWidget.js
@@ -1,21 +1,13 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Chart } from 'components/Chart/Chart';
 import { dummySignal } from 'lib/data/measurementData';
 
 export const FirstWidget = () => {
-  const [signalData, setSignalData] = useState(dummySignal);
-
-  function handleClick() {
-    setSignalData(dummySignal);
-  }
   return (
     <div className="w-360 h-640 flex justify-center items-center m-1 p-4  border border-solid border-red-500/75 rounded bg-amber-100">
       <h1>Code</h1>
-      <button type="button" onClick={handleClick} className="w-full bg-red-500">
-        click
-      </button>
       <div className="w-full">
-        <Chart userData={signalData} />
+        <Chart userData={dummySignal} />
       </div>
     </div>
   );
