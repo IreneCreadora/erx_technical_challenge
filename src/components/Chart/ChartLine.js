8175d19433d67d5d993147d2bc93af37bc50cf4b
IreneCreadora <nargiza.dancer@gmail.com>
2023-03-14 12:44:12 +0000

    feature new chart for third widget

---
+++ src/components/Chart/ChartLine.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+} from 'chart.js';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip
+);
+
+export const ChartLine = ({ userData }) => {
+  const [signal, setSignal] = useState([]);
+  const [label, setLabel] = useState([]);
+
+  useEffect(() => {
+    function getSignal() {
+      const data = userData;
+      setSignal(data.map(item => item));
+      setLabel(data.map((elem, index) => index));
+    }
+
+    setTimeout(() => {
+      getSignal();
+    }, 10000);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const data = {
+    labels: label.map(item => item),
+    datasets: [
+      {
+        label: 'Signal',
+        data: signal.map(item => item),
+        borderColor: 'red',
+        pointBorderWidth: 0,
+        pointBackgroundColor: 'transparent',
+        borderWidth: 0.3,
+        tension: 0.5,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {},
+    gridLines: false,
+    scales: {
+      x: {
+        display: false,
+      },
+      y: {
+        display: false,
+      },
+    },
+  };
+
+  return (
+    <>{signal.length > 1 && <Line data={data} options={options}></Line>}</>
+  );
+};
